90ce5d7e6e0215d1edbc8c823b3ff648a21586a3
Gledbury <gledbury4@gmail.com>
2019-10-02 20:34:28 -0700

    Lab2 WIP

---
+++ Lab2/SportsCar.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Lab2
+{
+    public class SportsCar : Car
+    {
+        public string GetPetName()
+        { PetName = "Fred";
+            return PetName;
+        }
+    }
+}
